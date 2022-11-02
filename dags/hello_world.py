ad599c538105bef3da23e4a3f18a8f7fb7dbb947
Aldo Agunin <aldoagunin@gmail.com>
2022-11-02 17:08:54 -0300

    sql completos con columnas NULL y nombres requeridos

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+
 def print_hello():
-  print('¡Hola Mundo!')
+    print('Hola Mundo!')
+
 dag = DAG('hello_world', description='Hola Mundo DAG',
-schedule_interval='* * * * *',
-start_date=datetime(2017, 3, 20),
-catchup=False,)
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)example_dag_basic = example_dag_basic()
+        schedule_interval='* * * * *',
+        start_date=datetime(2017, 3, 20),
+        catchup=False,)
+
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+# hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)

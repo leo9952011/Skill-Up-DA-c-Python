e80fcd78110794594c4c161a1af4f6913670e94e
Aldo Agunin <aldoagunin@gmail.com>
2022-11-02 19:11:56 -0300

    Merge branch 'gaby' of https://github.com/leo9952011/Skill-Up-DA-c-Python into aldo
    # Conflicts:
    #	.env.example
    #	.gitignore

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -3,11 +3,11 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
-    print('Hola Mundo!')
+    print('Hello World!')
 
 dag = DAG('hello_world', description='Hola Mundo DAG',
         schedule_interval='* * * * *',
-        start_date=datetime(2017, 3, 20),
+        start_date=datetime(2021, 10, 20),
         catchup=False,)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)

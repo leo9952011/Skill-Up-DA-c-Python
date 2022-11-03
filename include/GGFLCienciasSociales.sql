a55e4669628fd51ca2f8bea9186508cc672c0763
Leandro Pardo <leo95952011@gmail.com>
2022-11-03 18:01:09 -0300

    merge con gaby

---
+++ include/GGFLCienciasSociales.sql
@@ -0,0 +1,16 @@
+SELECT  universities AS university, 
+        careers AS career, 
+		inscription_dates AS inscription_date, 
+		NULL AS first_name,
+		names AS last_name, 
+		sexo AS gender, 
+		birth_dates AS birth_date, 
+		NULL AS age,
+		NULL AS postal_code,
+		locations AS location,
+		emails AS email
+FROM 
+	lat_sociales_cine lsc 
+WHERE 
+	lsc.universities = '-FACULTAD-LATINOAMERICANA-DE-CIENCIAS-SOCIALES'
+	AND TO_DATE(lsc.inscription_dates,'DD-MM-YYYY') BETWEEN  '2020-09-01' AND '2021-02-01';
---
+++ include/GGUKennedy.sql
@@ -0,0 +1,16 @@
+SELECT  universidades AS university,
+		carreras AS career, 
+		fechas_de_inscripcion AS inscription_date, 
+		NULL AS first_name,
+		nombres AS last_name, 
+		sexo AS gender, 
+		fechas_nacimiento AS birth_date,
+		NULL AS age, 
+		codigos_postales AS postal_code,
+		NULL AS location,
+		emails AS email
+FROM 
+    uba_kenedy uk 
+WHERE 
+    uk.universidades = 'universidad-j.-f.-kennedy'
+    AND TO_DATE(uk.fecha_de_inscripcion,'YY-MON-DD') BETWEEN '2020-09-01' AND '2021-02-01';

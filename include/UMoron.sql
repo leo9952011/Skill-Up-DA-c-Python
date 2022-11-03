ccd05b85637256843573b30b802f2000db7b889b
NazarenoD <estan.riff@gmail.com>
2022-11-03 10:59:55 -0300

    final sql files Naza group F

--- include/UMoron.sql
+++ include/UMoron.sql
@@ -1,4 +1,14 @@
-SELECT nombrre, sexo, eemail, nacimiento, universidad, fechaiscripccion, carrerra, codgoposstal
+SELECT 
+	nombrre AS last_name,
+	NULL AS first_name,
+	sexo AS gender,
+	eemail AS email,
+	nacimiento,
+	NULL AS age,
+	universidad AS university,
+	fechaiscripccion AS inscription_date,
+	carrerra AS career,
+	codgoposstal AS postal_code
 	FROM public.moron_nacional_pampa 
 	WHERE universidad='Universidad de morón' AND
 	TO_DATE(fechaiscripccion,'DD/MM/YYYY') BETWEEN '2020-09-01' AND '2021-02-01';
--- include/UNRioCuarto.sql
+++ include/UNRioCuarto.sql
@@ -1,4 +1,15 @@
-SELECT names, sexo, email, fechas_nacimiento, univiersities, inscription_dates, carrera, localidad 
+SELECT 
+	names AS last_name,
+	NULL AS first_name,
+	sexo AS gender,
+	email AS email,
+	fechas_nacimiento,
+	NULL AS age,
+	univiersities AS university,
+	inscription_dates AS inscription_date,
+	carrera AS career,
+	localidad AS location 
+
 	FROM public.rio_cuarto_interamericana
 	WHERE univiersities='Universidad-nacional-de-río-cuarto' AND
 	TO_DATE(inscription_dates,'YY/MON/DD') BETWEEN '2020-09-01' AND '2021-02-01';

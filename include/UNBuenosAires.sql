7ed2a97cd98478cca64f2cecdb9377be0a921cf1
Leandro Pardo <99141620+leo9952011@users.noreply.github.com>
2022-11-03 11:08:40 -0300

    Delete UNBuenosAires.sql

--- include/UNBuenosAires.sql
+++
@@ -1,4 +0,0 @@
-select universidades, carreras, fechas_de_inscripcion, nombres, sexo, codigos_postales, emails  
-from uba_kenedy uk 
-where to_date(fechas_de_inscripcion , 'yy-Mon-dd') between '2020-09-01' and '2021-02-01'
-and universidades = 'universidad-de-buenos-aires';

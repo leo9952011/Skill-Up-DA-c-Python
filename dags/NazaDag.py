3d5ff9183135cdf1f6d19bf0880a0fc03e751ef5
NazarenoD <estan.riff@gmail.com>
2022-11-04 09:22:50 -0300

    Logging and save CSV files

--- dags/NazaDag.py
+++ dags/NazaDag.py
@@ -4,6 +4,92 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
+import logging
+
+import pandas as pd
+from decouple import config as settings
+from sqlalchemy import create_engine
+from typing import List
+
+
+
+
+
+#############################LOGER#############################
+
+def create_logger():  
+
+    logger = logging.getLogger('models_logger') 
+    logger.setLevel(logging.INFO) 
+    logfile = logging.FileHandler(r'dags/logs/log_file.log') 
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(fmt) 
+    logfile.setFormatter(formatter) 
+    logger.addHandler(logfile) 
+      
+    return logger 
+
+log = create_logger()
+
+#############################FUNCTIONS#############################
+
+##########SQL EXECUTE##########
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings('DB_USERNAME')}:{settings('DB_PASSWORD')}@{settings('DB_HOST')}/{settings('DB_NAME')}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+def get_query(path:List):
+    for i in path:
+        try:
+            with open(i, 'r',encoding="utf-8") as myfile:
+                data = myfile.read()
+                data.replace('\n', ' ')
+                log.info(f"query in process {i} ")
+                return str(data)
+        except:
+            log.warning(f"SyntaxError: invalid syntax for query {i} ")
+            pass
+
+
+
+def sql_execute():
+    log.info('INIT sql EXECUTE')
+    
+    path_rio_Cuarto = get_query([r'include\UNRioCuarto.sql'])
+    path_Moron = get_query([r'include\UMoron.sql'])
+    
+    log.info('conection to database')
+    try:
+        df_mr = pd.read_sql(path_Moron,con = engine)
+        log.info('available dataframe U-Moron')
+        
+        df_mr.to_csv(r'files/GFUMoron.csv')
+        log.info('available csv U-Moron')
+        
+    except:
+        log.warning(f'Error in {path_Moron}')
+        
+        
+    try:
+        df_rc = pd.read_sql(path_rio_Cuarto,con = engine)
+        log.info('available dataframe U-Rio-Cuarto')
+        
+        df_rc.to_csv(r'files/GFUNRioCuarto.csv')
+        log.info('available csv U-Rio-Cuarto')
+        
+    except:
+        log.warning(f'Error in {path_rio_Cuarto}')
+        
+
+
+def data_processing():
+    log.info('procesamiento en pandas')
+def data_upload():
+    log.info('carga de datos a S3')
+
+
+
+#############################DAG#############################
+
 default_args = {
     'owner':'Nazareno',
     'depends_on_past':False,
@@ -14,12 +100,7 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-def sql_execute():
-    print('ejecucion de archivos sql')
-def data_processing():
-    print('procesamiento en pandas')
-def data_upload():
-    print('carga de datos a S3')
+
 
 with DAG(
     'Naza-Dag',

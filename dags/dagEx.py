28a8e5bdb5aab7f2bd18aca49e4c245b4fbac12c
NazarenoD <estan.riff@gmail.com>
2022-11-08 00:43:23 -0300

    make pandas dataframe

--- dags/dagEx.py
+++ dags/dagEx.py
@@ -1,53 +1,83 @@
 import logging
 from datetime import timedelta
+
 from airflow.utils.dates import days_ago
+from airflow.decorators import (dag, task)
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-def create_logger():  
-
-    logger = logging.getLogger('Dag_logger') 
-    logger.setLevel(logging.INFO) 
-    logfile = logging.FileHandler(r'/usr/local/airflow/files/log_file.log') 
-    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    formatter = logging.Formatter(fmt) 
-    logfile.setFormatter(formatter) 
-    logger.addHandler(logfile) 
-      
-    return logger 
-
-log = create_logger()
-
-from airflow.decorators import (
-    dag, 
-    task
-    )
+import pandas as pd
+from typing import List
 
 @dag(
-    schedule=timedelta(seconds=15),
+    schedule=timedelta(minutes=60),
     start_date=days_ago(1),
     catchup=False,
     default_args={
         "retries": 5,
     },
-    tags=['example'])
-def dagNz():
+    tags=['DataProcessing'])
+def DataProcessing():
+    
     @task()
-    def data():
-        log.info("SQL Query")
-        with open(r'/usr/local/airflow/include/GFUNRioCuarto.sql', 'r') as myfile:
-            sql_query = myfile.read()
+    def dataRc():
+        
+        df = False
+        logging.info('preparing sql execution')
+        try:
+            with open(r'/usr/local/airflow/include/GFUNRioCuarto.sql', 'r') as myfile:
+                sql_query = myfile.read()
             print(sql_query)
-            
+            hook = PostgresHook(postgres_conn_id='pgconnectionAlkemy')
+            df = hook.get_pandas_df(sql = sql_query)
+            print(df.head())
+            print(type(df))       
+            df.to_csv(r'/usr/local/airflow/include/GFUNRioCuarto.csv')
+            logging.info('successful sql execution U.N.RIO-CUARTO')
+        except:
+            logging.warning('sql execution failed U.N.RIO-CUART')
+            pass
+        return df
+     
+    
+    @task()
+    def dataMr():
+        
+        df = False
+        logging.info('preparing sql execution')
+        try:
+            with open(r'/usr/local/airflow/include/GFUMoron.sql', 'r') as myfile:
+                sql_query = myfile.read()
+            print(sql_query)
+            hook = PostgresHook(postgres_conn_id='pgconnectionAlkemy')
+            df = hook.get_pandas_df(sql = sql_query)
+            print(df.head())
+            print(type(df))    
+            df.to_csv(r'/usr/local/airflow/include/GFUMoron.csv')
+            df = pd.read_csv(r'/usr/local/airflow/include/GFUMoron.csv') 
+            logging.info('veamos si existe veamos si existeveamos si existeveamos si existeveamos si existeveamos si existe')
+            print(df.head())       
+            logging.info('successful sql execution U.N.RIO-CUARTO')
+        except:
+            logging.warning('sql execution failed U.N.RIO-CUART')
+            pass
+        return df
+        
+        
         
     @task()
-    def printwarning():
-        log.warning("log de warning")
+    def transformation(dfr:List):
+        for i in dfr:
+            if i:
+                logging.info('Pandas process')
+            else:
+                logging.warning("log de warning")
     
     @task()
     def printcritical():
-        log.critical("log error critico")
+        logging.critical("log error critico")
         
-    data()
-    printwarning()
+    dfr = [dataRc(), dataMr()]
+    transformation(dfr)
     printcritical()
 
-dagNz = dagNz()
+DataProcessingDag = DataProcessing()

6ffa882b0f8a47bec92277c73bb38f922607ebfa
gonzalezgbr <gonzalezgbr@gmail.com>
2022-11-10 09:10:01 -0300

    Refactor task definitions.

--- dags/GGFLCienciasSociales_dag_etl.py
+++ dags/GGFLCienciasSociales_dag_etl.py
@@ -4,9 +4,34 @@ from datetime import datetime
 from pathlib import Path
 
 from airflow import DAG
-from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+def extract_task():
+    """Get FLCienciasSociales data from remote postgres DB and save to csv file locally."""
+
+    local_basepath = Path(__file__).resolve().parent.parent
+
+    # Read SQL query
+    sql_filepath = local_basepath / 'include/GGFLCienciasSociales.sql'
+    with open(sql_filepath, encoding='utf8') as sql_file:
+        sql_str = sql_file.read()
+
+    # connect to remote postgres DB via hook
+    pg_hook = PostgresHook(postgres_conn_id="alkemy_db")
+    uni_df = pg_hook.get_pandas_df(sql=sql_str)
+
+    # save df data to csv locally
+    csv_filepath = local_basepath / 'files/GGFLCienciasSociales_select.csv'
+    uni_df.to_csv(csv_filepath, sep=',', header=True, encoding='utf-8')
+
+
+def transform_task():
+    """Load data from local csv, normalize with pandas and save to txt file locally."""
+
+    print(f'Transform all data task placeholder...')
 
 
 with DAG('GGFLCienciasSociales_dag_etl',
@@ -15,40 +40,17 @@ with DAG('GGFLCienciasSociales_dag_etl',
         schedule_interval='@hourly',
         ) as dag:
 
-    @task(task_id='extract', retries=5)
-    def extract():
-        """
-        Get FLCienciasSociales data from remote postgres DB and save to csv file locally.
-        files/GGFLCienciasSociales_select.csv
-        """
-        
-        local_basepath = Path(__file__).resolve().parent.parent
+    extract = PythonOperator(task_id='extract',
+                             python_callable=extract_task,
+                             retries=5)
 
-        # Read SQL query
-        sql_filepath = local_basepath / 'include/GGFLCienciasSociales.sql'
-        with open(sql_filepath, encoding='utf8') as sql_file:
-            sql_str = sql_file.read()
+    transform = PythonOperator(task_id='transform',
+                             python_callable=transform_task,
+                             retries=5)
 
-        # connect to remote postgres DB via hook
-        pg_hook = PostgresHook(postgres_conn_id="alkemy_db")
-        uni_df = pg_hook.get_pandas_df(sql=sql_str)
-
-        # save df data to csv locally
-        csv_filepath = local_basepath / 'files/GGFLCienciasSociales_select.csv'
-        uni_df.to_csv(csv_filepath, sep=',', header=True, encoding='utf-8')
-
-
-    @task(task_id='transform', retries=5)
-    def transform(dataset1):
-        """Load data from local csv, normalize with pandas and save to txt file locally."""
- 
-        # Will use PythonOperator
-        print(f'Transform all data task placeholder... {dataset1}')
-    
     # Load FLACSO data from local txt to S3. Uses LocalFilesystemToS3Operator Operator.
-    load = EmptyOperator(
-                    task_id='load',
-                    retries=5)
+    load = EmptyOperator(task_id='load',
+                         retries=5)
 
     # set task dependencies
-    transform(extract()) >> load
+    transform >> extract >> load
--- dags/GGUKennedy_dag_etl.py
+++ dags/GGUKennedy_dag_etl.py
@@ -4,9 +4,34 @@ from datetime import datetime
 from pathlib import Path
 
 from airflow import DAG
-from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+def extract_task():
+    """Get UKennedy data from remote postgres DB and save to csv file locally."""
+
+    local_basepath = Path(__file__).resolve().parent.parent
+
+    # Read SQL query
+    sql_filepath = local_basepath / 'include/GGUKennedy.sql'
+    with open(sql_filepath, encoding='utf8') as sql_file:
+        sql_str = sql_file.read()
+
+    # connect to remote postgres DB via hook
+    pg_hook = PostgresHook(postgres_conn_id="alkemy_db")
+    uni_df = pg_hook.get_pandas_df(sql=sql_str)
+
+    # save df data to csv locally
+    csv_filepath = local_basepath / 'files/GGUKennedy_select.csv'
+    uni_df.to_csv(csv_filepath, sep=',', header=True, encoding='utf-8')
+
+
+def transform_task():
+    """Load data from local csv, normalize with pandas and save to txt file locally."""
+
+    print(f'Transform all data task placeholder..')
 
 
 with DAG('GGUKennedy_dag_etl',
@@ -15,37 +40,13 @@ with DAG('GGUKennedy_dag_etl',
         schedule_interval='@hourly',
         ) as dag:
 
+    extract = PythonOperator(task_id='extract',
+                             python_callable=extract_task,
+                             retries=5)
 
-    @task(task_id='extract', retries=5)
-    def extract():
-        """
-        Get UKennedy data from remote postgres DB and save to csv file locally.
-        files/GGUKennedy_select.csv
-        """
-        
-        local_basepath = Path(__file__).resolve().parent.parent
-
-        # Read SQL query
-        sql_filepath = local_basepath / 'include/GGUKennedy.sql'
-        with open(sql_filepath, encoding='utf8') as sql_file:
-            sql_str = sql_file.read()
-
-        # connect to remote postgres DB via hook
-        pg_hook = PostgresHook(postgres_conn_id="alkemy_db")
-        uni_df = pg_hook.get_pandas_df(sql=sql_str)
-
-        # save df data to csv locally
-        csv_filepath = local_basepath / 'files/GGUKennedy_select.csv'
-        uni_df.to_csv(csv_filepath, sep=',', header=True, encoding='utf-8')
-        
-
-    @task(task_id='transform', retries=5)
-    def transform(dataset1):
-        """Load data from local csv, normalize with pandas and save to txt file locally."""
-
-        # Will use PythonOperator
-        print(f'Transform all data task placeholder... {dataset1}')
-        
+    transform = PythonOperator(task_id='transform',
+                             python_callable=transform_task,
+                             retries=5)
 
     # Load FLACSO data from local txt to S3. Uses LocalFilesystemToS3Operator Operator.
     load = EmptyOperator(
@@ -53,4 +54,4 @@ with DAG('GGUKennedy_dag_etl',
                     retries=5)
 
     # set task dependencies
-    transform(extract()) >> load
+    transform >> extract >> load

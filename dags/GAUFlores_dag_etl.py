c6363250987504ced70318eae6e165d46f1e6728
mm24 <matiasvozzi55@gmail.com>
2022-11-11 23:28:52 -0300

    Added extraction task to dag files, docker-compose.override.yml with mount to /files, /plugins/constants.py, and /plugins/__init__.py to allow importing.

--- dags/GAUFlores_dag_etl.py
+++ dags/GAUFlores_dag_etl.py
@@ -3,30 +3,80 @@ import os
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import pandas as pd
+
+from plugins.constants import DATASETS_DIR, FILES_DIR, INCLUDE_DIR, \
+    POSTGRES_CONN_ID
 
 
 # Constants
 UNIVERSITY_ID = 'GAUFlores'
+SQL_PATH = os.path.join(INCLUDE_DIR, f'{UNIVERSITY_ID}.sql')
+CSV_PATH = os.path.join(FILES_DIR, f'{UNIVERSITY_ID}_select.csv')
+
+
+def extract_func(
+    university_id=UNIVERSITY_ID,
+    sql_path=SQL_PATH,
+    csv_path=CSV_PATH,
+    db_conn_id=POSTGRES_CONN_ID
+):
+    '''Read query statement from .sql file in INCLUDE_DIR, get data from
+    postgres db and save it as .csv in FILES_DIR
+
+    Parameters
+    ----------
+    university_id: string
+        Unique per-university identifier to set names of files, custom-
+        ize logging information, etc.  Default is local constant UNIVERS
+        ITY_ID.
+    sql_path: string or path object
+        System path to the .sql file containing the sql query statement.
+        Default is local constant SQL_PATH.
+    csv_path: string or path object
+        System path with location and name of the .csv file to be creat-
+        ed.  Default is local constant CSV_PATH.
+    db_conn_id: string
+        Db connection id as configured in Airflow UI (Admin -> Connect-
+        ions).  Default is plugins.constants.POSTGRES_CONN_ID.
+
+    Returns
+    -------
+    None.
+    '''
+
+    # Read sql query statement from .sql in INCLUDE_DIR
+    with open(sql_path, 'r') as f:
+        command = f.read()
+
+    # Get hook, load data into pandas df and save it as .csv in FILES_DIR
+    pg_hook = PostgresHook.get_hook(db_conn_id)
+    df = pg_hook.get_pandas_df(command)
+    df.to_csv(csv_path, index=False)
 
 
 with DAG(
     dag_id=f'{UNIVERSITY_ID}_dag_etl',
     description=f'ETL process for {UNIVERSITY_ID}',
     schedule_interval='@hourly',
-    start_date=datetime(2022, 11, 9),
+    start_date=datetime(2022, 11, 11),
     default_args={'retries': 5, 'retry_delay': timedelta(minutes=5)}
 ) as dag:
-    # Read .sql in /include, save data as .csv in /files.  Use PythonOp-
-    # erator
-    extract = EmptyOperator(task_id='extract_func')
+    # Read .sql in INCLUDE_DIR, save data as .csv in FILES_DIR
+    extract = PythonOperator(
+        task_id='extract_func',
+        python_callable=extract_func,
+    )
 
     # Apply requested formats for each column, add location or postal
-    # code from codigos_postales.csv in /assets, compute ages from
+    # code from codigos_postales.csv in ASSETS_DIR, compute ages from
     # birthdate, eliminate wrong age registries and save data as .txt in
-    # /datasets.  Use PythonOperator.
+    # DATASETS_DIR.  Use PythonOperator.
     transform = EmptyOperator(task_id='transform_func')
 
-    # Load .txt from /datasets, upload to S3.  Use PythonOperator.
+    # Load .txt from DATASETS_DIR, upload to S3.  Use PythonOperator.
     load = EmptyOperator(task_id='load_func')
 
     extract >> transform >> load
---
+++ plugins/constants.py
@@ -0,0 +1,8 @@
+import os
+
+WORKING_DIR = os.getcwd()
+DATASETS_DIR = os.path.join(WORKING_DIR, 'datasets')
+FILES_DIR = os.path.join(WORKING_DIR, 'files')
+INCLUDE_DIR = os.path.join(WORKING_DIR, 'include')
+
+POSTGRES_CONN_ID = 'alkemy_db'

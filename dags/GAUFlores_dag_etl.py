ead8b5de2b0e121467e68d0508720ab6e59eff09
mm24 <matiasvozzi55@gmail.com>
2022-11-15 03:55:39 -0300

    Modified dags up to extraction task. Added ETL_funcs module in GA_modules. Moved constants to GA_modules.

--- dags/GAUFlores_dag_etl.py
+++ dags/GAUFlores_dag_etl.py
@@ -4,11 +4,11 @@ import os
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 import pandas as pd
 
-from plugins.constants import DATASETS_DIR, FILES_DIR, INCLUDE_DIR, \
-    POSTGRES_CONN_ID
+from plugins.GA_modules.constants import FILES_DIR, INCLUDE_DIR
+from plugins.GA_modules.ETL_funcs import extract_func
+
 
 
 # Constants
@@ -17,57 +17,22 @@ SQL_PATH = os.path.join(INCLUDE_DIR, f'{UNIVERSITY_ID}.sql')
 CSV_PATH = os.path.join(FILES_DIR, f'{UNIVERSITY_ID}_select.csv')
 
 
-def extract_func(
-    university_id=UNIVERSITY_ID,
-    sql_path=SQL_PATH,
-    csv_path=CSV_PATH,
-    db_conn_id=POSTGRES_CONN_ID
-):
-    '''Read query statement from .sql file in INCLUDE_DIR, get data from
-    postgres db and save it as .csv in FILES_DIR
-
-    Parameters
-    ----------
-    university_id: string
-        Unique per-university identifier to set names of files, custom-
-        ize logging information, etc.  Default is local constant UNIVERS
-        ITY_ID.
-    sql_path: string or path object
-        System path to the .sql file containing the sql query statement.
-        Default is local constant SQL_PATH.
-    csv_path: string or path object
-        System path with location and name of the .csv file to be creat-
-        ed.  Default is local constant CSV_PATH.
-    db_conn_id: string
-        Db connection id as configured in Airflow UI (Admin -> Connect-
-        ions).  Default is plugins.constants.POSTGRES_CONN_ID.
-
-    Returns
-    -------
-    None.
-    '''
-
-    # Read sql query statement from .sql in INCLUDE_DIR
-    with open(sql_path, 'r') as f:
-        command = f.read()
-
-    # Get hook, load data into pandas df and save it as .csv in FILES_DIR
-    pg_hook = PostgresHook.get_hook(db_conn_id)
-    df = pg_hook.get_pandas_df(command)
-    df.to_csv(csv_path, index=False)
-
-
 with DAG(
     dag_id=f'{UNIVERSITY_ID}_dag_etl',
     description=f'ETL process for {UNIVERSITY_ID}',
     schedule_interval='@hourly',
-    start_date=datetime(2022, 11, 11),
-    default_args={'retries': 5, 'retry_delay': timedelta(minutes=5)}
+    start_date=datetime(2022, 11, 15)
 ) as dag:
     # Read .sql in INCLUDE_DIR, save data as .csv in FILES_DIR
     extract = PythonOperator(
         task_id='extract_func',
         python_callable=extract_func,
+        op_kwargs={
+            'university_id': UNIVERSITY_ID,
+            'sql_path': SQL_PATH,
+            'csv_path': CSV_PATH,
+        },
+        retries=5
     )
 
     # Apply requested formats for each column, add location or postal
---
+++ plugins/GA_modules/ETL_funcs.py
@@ -0,0 +1,43 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+from plugins.GA_modules.constants import POSTGRES_CONN_ID
+
+
+def extract_func(
+    university_id,
+    sql_path,
+    csv_path,
+    db_conn_id=POSTGRES_CONN_ID
+):
+    """Read query statement from .sql file in INCLUDE_DIR, get data from
+    postgres db and save it as .csv in FILES_DIR
+
+    Parameters
+    ----------
+    university_id: string
+        Unique per-university identifier to set names of files, custom-
+        ize logging information, etc.  Default is local constant UNIVERS
+        ITY_ID.
+    sql_path: string or path object
+        System path to the .sql file containing the sql query statement.
+        Default is local constant SQL_PATH.
+    csv_path: string or path object
+        System path with location and name of the .csv file to be creat-
+        ed.  Default is local constant CSV_PATH.
+    db_conn_id: string
+        Db connection id as configured in Airflow UI (Admin -> Connect-
+        ions).  Default is plugins.constants.POSTGRES_CONN_ID.
+
+    Returns
+    -------
+    None.
+    """
+
+    # Read sql query statement from .sql in INCLUDE_DIR
+    with open(sql_path, 'r') as f:
+        command = f.read()
+
+    # Get hook, load data into pandas df and save it as .csv in FILES_DIR
+    pg_hook = PostgresHook.get_hook(db_conn_id)
+    df = pg_hook.get_pandas_df(command)
+    df.to_csv(csv_path, index=False)

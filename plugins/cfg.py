f699ae630c8ec9611e5681ad6c85fa0bf3269990
MauMN <81426396+MauMN@users.noreply.github.com>
2022-11-15 09:57:08 -0300

    delete files

--- plugins/cfg.py
+++
@@ -1,7 +0,0 @@
-from decouple import config
-
-DB_USERNAME = config("DB_USERNAME")
-DB_HOST = config("DB_HOST")
-DB_PORT = config("DB_PORT")
-DB_NAME = config("DB_NAME")
-DB_PASSWORD = config("DB_PASSWORD")
